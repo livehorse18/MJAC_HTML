a019ac630c2095e3faf04f810b19fbdae983d245
livehorse18 <64065548+livehorse18@users.noreply.github.com>
2020-06-10 01:05:14 +0900

    アップロード

---
+++ MJAC_HTML/js/majan.js
@@ -0,0 +1,10 @@
+"use strict";
+
+export const VERSION = '1.3.6';
+export const Shoupai = require('./majiang/shoupai');
+export const Shan = require('./majiang/shan');
+export const He = require('./majiang/he');
+export const Util = require('./majiang/util');
+export const Game = require('./majiang/game');
+export const Player = require('./majiang/player');
+export const SuanPai = require('./majiang/suanpai');
